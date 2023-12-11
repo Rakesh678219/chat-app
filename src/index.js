2725cc23e15ead9cfcd167b45f501a908a0d53c3
rakesh <rpeddamallu@juniper.net>
2023-12-11 18:46:35 +0530

    basic socket setup with couter

--- src/index.js
+++ src/index.js
@@ -2,11 +2,25 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
 
 const publicDirectoryPath = path.join(__dirname, "../public");
 
+const server = http.createServer(app);
+const io = socketio(server);
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
-  console.log("listening on port " + port);
+let count = 0;
+io.on("connection", (socket) => {
+  console.log("New web socket connection");
+  socket.emit("countUpdated", count);
+  socket.on("increment", () => {
+    count++;
+    io.emit("countUpdated", count);
+  });
+});
+
+server.listen(port, () => {
+  console.log("Server listening on port " + port);
 });
